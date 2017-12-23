09c49e5db4ebe149211d1621a43065d23472c874
tadano kakasi <basyavasya.honmono@gmail.com>
2017-12-24 00:38:39 +0900

    条件変更 4回中3回検出したら音を鳴らす

--- testing_utils/videotest.py
+++ testing_utils/videotest.py
@@ -97,6 +97,7 @@ class VideoTest(object):
         fps = "FPS: ??"
         prev_time = timer()
         kaisuu=0
+        all=0
         targets=[15,12,7,2,6,8,14]
         names=["person","dog","car","bicycle","bus","cat","motorbike"]
         counts=[0]*len(targets)
@@ -155,16 +156,18 @@ class VideoTest(object):
                 print("top:",top_label_indices)
                 
                 #beep
+                all=all+1
                 found=False
                 for i, target in enumerate(targets,0) :
                     print(names[i],targets[i] in top_label_indices)
                     if (target in top_label_indices):
                         counts[i]=counts[i]+1
-                        if counts[i]==3 :
-                           counts[i]=0
-                           found=True
-                    else:
+                    if all==4 :
+                        if counts[i]/all >= 3/4 :
+                            found=True
                         counts[i]=0
+                if all==4 :
+                    all=0
                 if found :
                     print ('\007')
 
